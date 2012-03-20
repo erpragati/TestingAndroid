88842eece699145507ec1e76e9f4f1cd860a3d70
G. Blake Meike <blake.meike@gmail.com>
2012-03-20 11:35:13 -0700

    Slides for part II

--- AndroidTest/src/net/callmeike/android/testing/CalcActivityTest.java
+++ AndroidTest/src/net/callmeike/android/testing/CalcActivityTest.java
@@ -3,11 +3,6 @@
 package net.callmeike.android.testing;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.UiThreadTest;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import net.callmeike.android.testing.calc.Calculator;
 
 
 /**
@@ -16,7 +11,6 @@ import net.callmeike.android.testing.calc.Calculator;
  * @author <a href="mailto:blake.meike@gmail.com">G. Blake Meike</a>
  */
 public class CalcActivityTest extends ActivityInstrumentationTestCase2<TestTargetActivity> {
-    private TestTargetActivity activity;
 
     /**
      *
@@ -29,32 +23,5 @@ public class CalcActivityTest extends ActivityInstrumentationTestCase2<TestTarge
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        activity = getActivity();
-    }
-
-    /**
-     *
-     */
-    @UiThreadTest
-    public void testGetAndClear() {
-        EditText enter = (EditText) activity.findViewById(R.id.input);
-
-        assertNotNull(enter);
-
-        final Integer content = Integer.valueOf(666);
-        enter.setText(content.toString());
-
-        assertEquals(content, activity.getAndClear());
-        assertEquals("", enter.getText().toString());
-    }
-
-    /**
-     *
-     */
-    @UiThreadTest
-    public void testUnderflow() {
-        activity.eval(Calculator.Op.ADD);
-        TextView top = (TextView) activity.findViewById(R.id.top);
-        assertEquals("Stack underflow", top.getText().toString());
     }
 }
